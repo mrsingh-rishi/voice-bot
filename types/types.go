af9091df823f4b6d57e4f0b7f4fc3e9ee1c18e8c
Rishi Singh <rs3949427@gmail.com>
2025-05-20 00:12:56 +0530

    Created types for the trascription result

--- types/types.go
+++ types/types.go
@@ -1,7 +1,16 @@
 package types
 
+import "context"
+
 type TranscriptionChannel struct {
 	Transcription string
 	Confidence    float64
 	Final         bool
+}
+
+type TranscriptionResult struct {
+	Transcription string
+	Confidence    float64
+	Final         bool
+	Ctx           context.Context
 }
